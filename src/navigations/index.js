5f1736c72271d015c841667b011a4bf0babba80a
jess0419kim <jessi.kim@taction.io>
2023-02-23 04:45:59 +0000

    auth to main transition

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,15 +1,28 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigation from './authNavigation';
 import MainNavigation from './mainNavigation';
+import { supabase } from '../configs/supabaseConfig';
 
 const Navigations = () => {
 
-    const [isLogin, setIsLogin] = useState(false)
+    const [session, setSession] = useState(null);
+	
+	
+	useEffect( ()=>{
+		 supabase.auth.getSession().then(({data:{session}}) => {
+		  setSession(session)	
+		 })
+		
+		supabase.auth.onAuthStateChange( async(event, session) => {
+		   setSession(session)
+		})
+	},[]);
+  
 
     return(
         <NavigationContainer>
-            {isLogin? <MainNavigation />: <AuthNavigation />} 
+            {session? <MainNavigation />: <AuthNavigation />} 
         </NavigationContainer>
     )
 }
