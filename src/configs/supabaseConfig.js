9c5b94c76a5cac3c199c5e0f260c6751b87c447e
jess0419kim <jessi.kim@taction.io>
2023-02-23 07:06:15 +0000

    dotenv

--- src/configs/supabaseConfig.js
+++ src/configs/supabaseConfig.js
@@ -1,11 +1,12 @@
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import {SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 
+const supabaseUrl = SUPABASE_URL
+const supabaseAnonKey = SUPABASE_ANON_KEY
 
-const supabaseUrl = "https://srrthevwejcoekxlvjmp.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNycnRoZXZ3ZWpjb2VreGx2am1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAwNTExMjIsImV4cCI6MTk4NTYyNzEyMn0.AOTgXJMyecI7yo6oWiJeeHWGMwsOOVdjEMoh221VzNM"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
