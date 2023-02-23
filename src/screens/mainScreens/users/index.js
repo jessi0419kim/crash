04c4f40b067847266c5a90ed4229c242bb0b7e1e
jess0419kim <jessi.kim@taction.io>
2023-02-23 05:18:35 +0000

    signOut with Supabase

--- src/screens/mainScreens/users/index.js
+++ src/screens/mainScreens/users/index.js
@@ -1,22 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { Color } from '../../../../styles/global';
+import { supabase } from '../../../configs/supabaseConfig';
 
 const Users = () => {
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const signOutWithSupabase = async() => {
+    setIsLoading(prev=>!prev)
+    await supabase.auth.signOut()
+    setIsLoading(prev=>!prev)
+  } 
+
+
   return (
-      <View style={styles.container}>
-        <Text>Users</Text>
+      <View  style={{flex:1, backgroundColor: Color.white, paddingHorizontal: 20}}>
+              <TouchableOpacity onPress={signOutWithSupabase}>
+                       <View  style={{flexDirection: 'row', 
+                                      justifyContent: 'center', 
+                                      alignItems: 'center',
+                                      height: 52, borderRadius: 24, marginVertical: 24,
+                                      backgroundColor: Color.primary}} >
+                            {isLoading
+                            ? <ActivityIndicator size="small" color={Color.white}/>
+                            : <Text style={{ fontSize: 18,  fontWeight: '600', color: Color.white}}>Sign Out</Text>}
+                         </View>
+              </TouchableOpacity >
       </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 
 
 export default Users
